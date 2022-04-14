42b007c8138ea751f24f93b63722ab9370f583a6
Pranav-Bobde <bobdep31@gmail.com>
2022-04-15 00:28:55 +0530

    created project with react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HomePage from "./homepage.component";
+import './homepage.styles.scss'
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
